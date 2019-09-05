5aa9730a04d4723dd045827a24ab4f2cdd6fd1c9
Oscar Ponce <oscarponce.7@yahoo.com>
2019-09-05 11:30:56 -0500

    Created Room

---
+++ HypSuite.Domain/Models/Bed.cs
@@ -0,0 +1,8 @@
+namespace HypSuite.Domain.Models
+{
+    public class Bed
+    {
+        public int BedID{get;set;}
+        public string BedSize{get;set;}
+    }
+}
---
+++ HypSuite.Domain/Models/Room.cs
@@ -0,0 +1,13 @@
+namespace HypSuite.Domain.Models
+{
+    public class Room
+    {
+        public int RoomID {get;set;}
+        public int MaxCapacity { get; set; }
+        //public List<Bed> Beds { get; set; }
+        public int NumberOfBathrooms {get;set;}
+        public bool IsSmoking { get; set; }
+        public int SizeSqFt {get;set;}
+        public decimal DailyRate{get;set;}
+    }
+}
