ff38538c5c9020abb7582f0c83cfca0013789010
daniel ghazvini <danielghazvini@gmail.com>
2019-09-05 14:32:05 -0500

    Layout update

--- HypSuite.Client/Controllers/PortalController.cs
+++ HypSuite.Client/Controllers/PortalController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -15,22 +15,7 @@ namespace HypSuite.Client.Controllers
             return View();
         }
 
-        public IActionResult ClientLogin()
-        {
-          return View();
-        }
-
-        public IActionResult AdminLogin()
-        {
-          return View();
-        }
-
-        public IActionResult About()
-        {
-          return View();
-        }
-
-        public IActionResult Privacy()
+        public IActionResult ClientPortal()
         {
           return View();
         }
