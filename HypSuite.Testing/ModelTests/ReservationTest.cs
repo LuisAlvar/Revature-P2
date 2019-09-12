0aaed893dd56291c24f184ebb16b02db0ed634b6
Oscar Ponce <oscargeo.ponce@gmail.com>
2019-09-11 21:08:27 -0500

    new files

---
+++ HypSuite.Testing/ModelTests/ReservationTest.cs
@@ -0,0 +1,18 @@
+using System.xunit;
+
+namespace HypSuite.Testing.ModelTests
+{
+    public class ReservationTest
+    {
+      [Fact]
+      public void TestRoomList()
+      {
+      //Given
+      
+      //When
+      
+      //Then
+      }
+        
+    }
+}
